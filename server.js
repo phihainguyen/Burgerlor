3eb771c959bbe1e73f158fb0f0bc415684238cb6
Phi Hai Nguyen <phihai93@gmail.com>
2019-04-08 16:37:59 -0700

    update

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.set("view engine", "handlebars");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+app.use(express.static("public"));
+
 app.listen(PORT, function(){
     console.log("listening on port: "+ PORT)
 })
